ca9ada4b9a0150277105b02b903a1eb5182fcb27
Luyi Tian <tlytiger@hotmail.com>
2017-08-17 18:20:38 +1000

    moving to `SingleCellExperiment`

--- R/help.R
+++ R/help.R
@@ -1,13 +1,14 @@
-#'scPipe - single cell RNA-seq pipeline
+#' scPipe - single cell RNA-seq pipeline
 #'
-#'It demultiplex, it quality control, it plots:
+#' It demultiplex, it quality control, it plots:
 #'
 #'
-#'@author Luyi Tian <tian.l@wehi.edu.au>; Shian Su <su.s@wehi.edu.au>
-#'@docType package
-#'@name scPipe
-#'@import Rhtslib
-#'@import Rcpp
-#'@useDynLib scPipe
-#'@aliases scPipe scPipe-package
+#' @author Luyi Tian <tian.l@wehi.edu.au>; Shian Su <su.s@wehi.edu.au>
+#' @docType package
+#' @name scPipe
+#' @import Rhtslib
+#' @import SingleCellExperiment
+#' @import Rcpp
+#' @useDynLib scPipe
+#' @aliases scPipe scPipe-package
 NULL
