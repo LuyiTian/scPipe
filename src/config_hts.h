cf80f1d99e4e3b0c48d4e403443778a3bf94e00d
Hervé Pagès <hpages@fredhutch.org>
2018-05-22 20:12:09 -0700

    Path to htslib header files has changed in Rhtslib 1.13.1

--- src/config_hts.h
+++ src/config_hts.h
@@ -3,9 +3,9 @@
 #ifndef CONFIG_HTS_H
 #define CONFIG_HTS_H
 
-#include "sam.h"
-#include "bgzf.h"
-#include "kseq.h"
+#include "htslib/sam.h"
+#include "htslib/bgzf.h"
+#include "htslib/kseq.h"
 
 /*
 #include <htslib/sam.h>
