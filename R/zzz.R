ab222e06f7aa3e1ab67b20d36b2b5d63d2e90270
Phil Yang <yang.p@slurm-login02.hpc.wehi.edu.au>
2021-02-18 17:50:13 +1100

    feat: updated .onAttach() function to check for samtools and R version for MACSr

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,50 @@
 .onAttach <- function(...) {
   # packageStartupMessage("Some startup message")
   
+  
+  # Check if required packages are installed
+  list_of_packages <- c("tools", "tidyr", "locStra", "grid", "dplyr", "Matrix", "seqinr")
+  bioconductor_packages <- c("BiocParallel", "Rsubread", "rtracklayer", "GenomicAlignments", "GenomicRanges", "GenomicFeatures", "Rsamtools", "SingleCellExperiment")
+  
+  if (!requireNamespace("BiocManager", quietly = TRUE)) 
+    install.packages("BiocManager")
+  
+  req_bioc_packages <- bioconductor_packages[!(bioconductor_packages %in% installed.packages()[,"Package"])]
+  req_packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
+  
+  if(length(req_bioc_packages)) BiocManager::install(req_bioc_packages)
+  if(length(req_packages)) install.packages(req_packages, repo="http://cran.rstudio.com/")
+  
+  for (package in list_of_packages)
+    library(package, character.only = TRUE)
+  for (package in bioconductor_packages)
+    library(package, character.only = TRUE)
+  
+  if (!"MACSr" %in% installed.packages()[,"Package"]) {
+    if (paste0(R.version$major, ".", R.version$minor) > "4.1.0") {
+      devtools::install_github("macs3-project/MACSr")
+    } else {
+      cat("R version 4.1.0 or greater is required for MACSr which is needed for sc_atac_call_peaks.\n")
+    }
+  }
+  # Check for platform
   if(.Platform$OS.type != "unix") {
-    packageStartupMessage("Windows platform detected, sc_atac_peak_calling() function may not operate. Please call peaks outside the package.")  
-  } else{
-    packageStartupMessage("Linux/MacOS detected, please make sure the latest MACS2 is in path if need to run sc_atac_peak_calling()")
+    packageStartupMessage("Windows platform detected, sc_atac_peak_calling() and sc_atac_remove_duplicates function may not operate. Please call peaks and remove duplicate reads outside the package.")  
+  } else {
+    packageStartupMessage("Linux/MacOS detected")
+    
+    # Check if samtools is installed
+    message("Checking if samtools installed")
+    tryCatch(
+      {
+        system2("samtools", stdout = NULL, stderr = NULL)
+        message("samtools was located")
+      },
+      
+      warning = function(w) {
+        message("samtools was not located. Please make sure it is installed to run sc_atac_remove_duplicates")
+      }
+    )
   }
   
   
