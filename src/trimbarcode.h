7fab8adf5a58675f8a492411926f81a393e101ef
Oliver Voogd <olivervoogd@gmail.com>
2021-04-06 16:00:42 +1000

    removed bc_start and bc_length

--- src/trimbarcode.h
+++ src/trimbarcode.h
@@ -65,8 +65,6 @@ std::vector<int> sc_atac_paired_fastq_to_csv(
         char *fq3_fn,
         char *fq_out, 
         char *bc_fn, 
-        int start,
-        int length, 
         int umi_start,
         int umi_length,
         char *umi_in,
