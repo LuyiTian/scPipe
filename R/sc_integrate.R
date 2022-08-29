a2864848877fcf17a4eab63ccfcf60ebf0f43696
Phil Yang <yang.p@slurm-login02.hpc.wehi.edu.au>
2022-08-29 17:10:08 +1000

    added cell line info attachment to sc_integrate

--- R/sc_integrate.R
+++ R/sc_integrate.R
@@ -7,6 +7,7 @@
 #' @description Generates an integrated SCE object with scRNA-Seq and scATAC-Seq data produced by the scPipe pipelines
 #' @param sce_list A list of SCE objects, named with the corresponding technologies
 #' @param sce_column_to_barcode_files A list of files containing the barcodes for each tech (if not needed then give a `NULL` entry)
+#' @param cell_line_info A list of files, each of which contains 2 columns corresponding to the barcode and cell line for each tech (if not needed then provide a `NULL` entry)
 #' @param barcode_match_file A .csv file with columns corresponding to the barcodes for each tech
 #' @param output_folder The path to the output folder
 #' 
@@ -17,6 +18,7 @@
 #'    barcode_match_file = bc_match_file,
 #'    sce_column_to_barcode_files = list("RNA" = rna_bc_anno, "ATAC" = NULL),
 #'    rev_comp = list("RNA" = FALSE, "ATAC" = TRUE),
+#'    cell_line_info = list("RNA" = rna_cell_line_info, "ATAC" = atac_cell_line_info,)
 #'    output_folder = output_folder
 #'    )
 #' }  
@@ -24,9 +26,10 @@
 #' @export
 #'
 sc_integrate <- function(sce_list,
-                         barcode_match_file,
+                         barcode_match_file = NULL,
                          sce_column_to_barcode_files = NULL,
                          rev_comp = NULL,
+                         cell_line_info = NULL,
                          output_folder = NULL) {
   
   if(is.null(output_folder)) {
@@ -69,44 +72,67 @@ sc_integrate <- function(sce_list,
     names(sce_list) <- techs # add names back
   }
   
-  barcode_match_df <- read.csv(barcode_match_file, header = TRUE)
-  
-  # Check if the barcodes match up with those in the match file, and if not, try taking the reverse complement to see if it helps
-  sce_list <- lapply(seq_along(sce_list), function(i) {
-    sce <- sce_list[[i]]
-    tech <- names(sce_list)[[i]]
-    matched <- na.omit(match(colnames(sce), barcode_match_df[[tech]]))
-    if (length(matched)/length(colnames(sce)) < 0.2) {
-      cat("Less than 20% of", tech, "barcodes match with the barcodes in the barcode match file. Trying the reverse complement.\n")
-      rev_comp <- Biostrings::reverseComplement(DNAStringSet(colnames(sce))) %>% as.character()
-      new_matched <- na.omit(match(rev_comp, barcode_match_df[[tech]]))
-      if (length(new_matched)/length(colnames(sce)) <= length(matched)/length(colnames(sce))) {
-        cat("Reverse complement didn't yield higher proportion of matched barcodes so using existing barcodes.\n")
-      } else {
-        cat("Reverse complement yielded higher proportion of matched barcodes so using reverse complement\n")
-        colnames(sce) <- rev_comp
+  if (!is.null(barcode_match_file)) {
+    barcode_match_df <- read.csv(barcode_match_file, header = TRUE)
+    # Check if the barcodes match up with those in the match file, and if not, try taking the reverse complement to see if it helps
+    sce_list <- lapply(seq_along(sce_list), function(i) {
+      sce <- sce_list[[i]]
+      tech <- names(sce_list)[[i]]
+      matched <- na.omit(match(colnames(sce), barcode_match_df[[tech]]))
+      if (length(matched)/length(colnames(sce)) < 0.2) {
+        cat("Less than 20% of", tech, "barcodes match with the barcodes in the barcode match file. Trying the reverse complement.\n")
+        rev_comp <- Biostrings::reverseComplement(DNAStringSet(colnames(sce))) %>% as.character()
+        new_matched <- na.omit(match(rev_comp, barcode_match_df[[tech]]))
+        if (length(new_matched)/length(colnames(sce)) <= length(matched)/length(colnames(sce))) {
+          cat("Reverse complement didn't yield higher proportion of matched barcodes so using existing barcodes.\n")
+        } else {
+          cat("Reverse complement yielded higher proportion of matched barcodes so using reverse complement\n")
+          colnames(sce) <- rev_comp
+        }
       }
-    }
-    sce
-  })
-  names(sce_list) <- techs # add names back
+      sce
+    })
+    names(sce_list) <- techs # add names back
+  }
+  
+  
+  # Left join the cell line info
+  if (!is.null(cell_line_info)) {
+    cat("Attaching cell line information provided.\n")
+    sce_list <- lapply(seq_along(sce_list), function(i) {
+      sce <- sce_list[[i]]
+      tech <- names(sce_list)[[i]]
+      cell_line_file <- cell_line_info[[i]]
+      
+      if (!is.null(cell_line_file)) {
+        cli <- read.csv(cell_line_file, header=FALSE)
+        colnames(cli) <- c("barcode", "cell_line")
+        merged_cell_line_info <- base::merge(data.frame(barcode = colnames(sce)), cli, all.x = TRUE) %>% column_to_rownames("barcode")
+        reordered_cli <- merged_cell_line_info[colnames(sce), , drop=FALSE]
+        colData(sce) <- cbind(colData(sce), cell_line = reordered_cli$cell_line)
+      } 
+      sce
+    })
+    names(sce_list) <- techs # add names back
+  }
   
   # Outer join the column data
-  cat("Merging qc metrics\n")
-
-  qc_dfs <- lapply(seq_along(sce_list), function(i) {
-    sce <- sce_list[[i]]
-    tech <- techs[[i]]
-    qc <- data.frame(colData(sce)) %>% rename_with( ~ paste0(tech, "_", .x))
-    base::merge(qc, barcode_match_df, by.x = "row.names", by.y = techs[[i]], all.x = TRUE) %>% rename(!!tech := "Row.names")
-  })
-  merged_qc <- Reduce(function(x, y) base::merge(x, y, all = TRUE), qc_dfs)
+  if (!is.null(barcode_match_file)) { 
+    cat("Merging qc metrics\n")
+    qc_dfs <- lapply(seq_along(sce_list), function(i) {
+      sce <- sce_list[[i]]
+      tech <- techs[[i]]
+      qc <- data.frame(colData(sce)) %>% rename_with( ~ paste0(tech, "_", .x))
+      base::merge(qc, barcode_match_df, by.x = "row.names", by.y = techs[[i]], all.x = TRUE) %>% rename(!!tech := "Row.names")
+    })
+    merged_qc <- Reduce(function(x, y) base::merge(x, y, all = TRUE), qc_dfs)
+  }
   
   # Create MultiAssayExperiment
   cat("Creating MultiAssayExperiment\n")
   mae <- MultiAssayExperiment(experiments = sce_list)
   mae@metadata$scPipe$version <- packageVersion("scPipe") 
-  mae@metadata$scPipe$integrated_qc <- merged_qc
+  if (!is.null(barcode_match_file)) mae@metadata$scPipe$integrated_qc <- merged_qc
   
   # Save the MAE object
   # saveRDS(mae, file.path(output_folder, "scPipe_MAE_object.rds"))
@@ -119,10 +145,12 @@ sc_integrate <- function(sce_list,
 #' @title Generates UMAP of multiomic data
 #' @description Uses feature count data from multiple experiment objects to produce a UMAP 
 #' @param mae The MultiAssayExperiment object
+#' @param by_cell_line Whether to colour by the cell line. Note that should only be used if the cell line info is present in the object.
 #' @param output_file The path of the output file
 #' @export
 sc_mae_plot_umap <- function(mae,
-                          output_file = NULL) {
+                             by_cell_line = FALSE,
+                             output_file = NULL) {
   set.seed(123)
   
   umap_dfs <- lapply(seq_along(mae), function(i) {
@@ -170,15 +198,28 @@ sc_mae_plot_umap <- function(mae,
     
     df_umap$source <- tech
     
+    # Attach cell line info if available
+    if (!is.null(colData(experiments(mae)[[tech]])$cell_line)) {
+      sce_coldata <- colData(experiments(mae)[[tech]])[, c("cell_line"), drop=FALSE]
+      df_umap <- base::merge(df_umap, sce_coldata, by.x = "barcode", by.y = "row.names", all.x = TRUE) 
+    } else {
+      df_umap$cell_line <- NA
+    }
     df_umap
   })
   names(umap_dfs) <- names(mae)
   
   umap_data <- do.call(rbind, umap_dfs)
   
-  g <- ggplot(umap_data, aes(x = UMAP1, y = UMAP2)) +
-                     geom_point(aes(col = source), size = 0.5) +
-                     theme_bw(base_size = 14)
+  if (isTRUE(by_cell_line)) {
+    g <- ggplot(umap_data, aes(x = UMAP1, y = UMAP2)) +
+      geom_point(aes(col = cell_line), size = 0.5) +
+      theme_bw(base_size = 14)
+  } else {
+    g <- ggplot(umap_data, aes(x = UMAP1, y = UMAP2)) +
+      geom_point(aes(col = source), size = 0.5) +
+      theme_bw(base_size = 14)
+  }
   
   if (!is.null(output_file)) {
     if (file.exists(output_file)) {
